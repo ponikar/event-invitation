4a0399aa7e09f0ff844afe7720b76ec5611ac6a6
Darshan Ponikar <drsofficial3k@gmail.com>
2021-06-26 22:53:52 +0530

    Working with Responsive layout

--- src/components/Index/Hero/hero-section.component.jsx
+++ src/components/Index/Hero/hero-section.component.jsx
@@ -3,11 +3,11 @@ import { Calendar } from "react-feather"
 export const HeroSection = () => {
   return (
     <section className="flex flex-col justify-center mt-24 p-5 items-center">
-      <h1 className="lg:text-5xl bold md:text-3xl text-xl lg:w-5/12 md:w-7/12 sm:w-11/12 text-center">
+      <h1 className="lg:w-5/12 w-11/12 lg:text-5xl text-xl bold text-center">
         This will be a tagline which address people nicely
       </h1>
-      <PrimaryButton className="my-12"> Add to Calender <Calendar size={18} className="text-white ml-2" />  </PrimaryButton>
-      <img src="/assets/svgs/hero.svg" className="lg:w-4/12 w-11/12 my-8" />
+      <PrimaryButton className="my-12">Add to Calender <Calendar size={18} className="text-white ml-2" />  </PrimaryButton>
+      <img alt="Event" loading="lazy" src="/assets/svgs/hero.svg" className="lg:w-4/12 w-11/12 my-8" />
     </section>
   );
 };
