f3baba2db8562bed551de40a9df9332c47578054
Darshan Ponikar <drsofficial3k@gmail.com>
2021-06-26 20:56:05 +0530

    Adding Font Family

--- src/pages/index.page.js
+++ src/pages/index.page.js
@@ -4,6 +4,7 @@
 
 export const Index = () => {
     return <div>
-        Landing Page will be here!
+       <h1 className="bold text-4xl"> Landing Page will be here!</h1>
+       <h1 className="bold"> Landing Page will be here!</h1>
     </div>
 }
