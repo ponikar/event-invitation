d875c570c2fc4ff0259fb34cc2f4e4b8393f9ba4
Darshan Ponikar <drsofficial3k@gmail.com>
2021-06-29 21:52:17 +0530

    spliting to cols

--- src/components/About/member.component.jsx
+++ src/components/About/member.component.jsx
@@ -5,10 +5,10 @@ const IconProps = {
     size:16
 }
 
-export const Member = () => {
+export const Member = ({ last }) => {
   return (
-    <div className="w-full flex flex-col items-center text-center p-2">
-     <img src="/assets/svgs/circle.svg" className="w-28 object-cover my-2" />
+    <div className={`w-full flex flex-col items-center text-center p-2 ${last && "col-span-2"}`}>
+     <img src="/assets/svgs/circle.svg" className="w-32 object-cover my-2" />
       <h2 className="text-center"> Name </h2>
       <div className="flex text-grey text-sm justify-center w-full items-center">
         <p className="mx-2"> Role </p> |
--- src/pages/about.page.js
+++ src/pages/about.page.js
@@ -11,20 +11,20 @@ import { Member } from "../components/About/member.component";
 export const About = () => {
     return <PageBase>
         <HeaderTitle className="mt-24" title="Hello there! Meet with our Community." />
-        <Container className="grid sm:grid-cols-4 gap-2 sm:w-7/12 w-full">
+        <Container className="grid sm:grid-cols-4 grid-cols-2 gap-2 sm:w-7/12 w-full">
             <Member />
             <Member />
             <Member />
             <Member />
         </Container>
-        <Container className="grid sm:grid-cols-5  sm:w-8/12 w-full">
+        <Container className="grid sm:grid-cols-5 grid-cols-2 sm:w-8/12 w-full">
             <Member />
             <Member />
             <Member />
             <Member />
-            <Member />
+            <Member last />
         </Container>
-        <Container className="grid sm:grid-cols-4 gap-2 sm:w-7/12 w-full">
+        <Container className="grid sm:grid-cols-4 grid-cols-2 gap-2 sm:w-7/12 w-full">
             <Member />
             <Member />
             <Member />
