d898bb918ca420fe1e2c94c4afa9c9859ab7c6b6
Darshan Ponikar <drsofficial3k@gmail.com>
2021-07-03 21:03:43 +0530

    showing loading when signing process is going on

--- src/components/Common/Routes/guest.component.jsx
+++ src/components/Common/Routes/guest.component.jsx
@@ -2,35 +2,53 @@ import { useCallback, useState } from "react";
 import { doSignIn } from "../../../firebase/firebase.auth";
 import { useTitle } from "../../../hooks/useTitle.component";
 import { PrimaryButton } from "../Button/button.component";
+import { Loading } from "../Loading/loading.component";
 import { PageBase } from "../Page-Base/page-base.component";
 import SectionTitle from "../Typography/section-title.component";
 
 const DEFAULT_HELPERS = {
-    isLoading:false,
-    message: "We are excited to see you there!",
-    success: false,
-}
+  isLoading: false,
+  message: "We are excited to see you there!",
+  success: false,
+};
 
 export const Guest = () => {
-    const [helpers, setHelpers] = useState(DEFAULT_HELPERS);
-    useTitle("NXT.tech | Sign in")
+  const [helpers, setHelpers] = useState(DEFAULT_HELPERS);
+  useTitle("NXT.tech | Sign in");
   const signInWithGoogle = useCallback(async () => {
-      try {
-          setHelpers({ ...DEFAULT_HELPERS, isLoading: true });
-          await doSignIn();
-          setHelpers({ ...DEFAULT_HELPERS, message: "Redirecting...", success: true });
-          window.location.reload();
-      } catch(e) {
-        setHelpers({ ...DEFAULT_HELPERS, message: "Something went wrong! Reload the page.", success: false });
-         
-      }
+    try {
+      setHelpers({
+        ...DEFAULT_HELPERS,
+        message: "Loading! Please wait.",
+        isLoading: true,
+      });
+      await doSignIn();
+      setHelpers({
+        ...DEFAULT_HELPERS,
+        message: "Redirecting...",
+        success: true,
+      });
+      window.location.reload();
+    } catch (e) {
+      setHelpers({
+        ...DEFAULT_HELPERS,
+        message: "Something went wrong! Reload the page.",
+        success: false,
+      });
+    }
   }, []);
 
   return (
     <PageBase>
       <section className="flex flex-col justify-center mt-24 p-5 items-center">
         <SectionTitle title={helpers.message} />
-        <PrimaryButton onClick={signInWithGoogle} className="my-1">Sign in with Google </PrimaryButton>
+        {helpers.isLoading ? (
+          <Loading />
+        ) : (
+          <PrimaryButton onClick={signInWithGoogle} className="my-1">
+            Sign in with Google{" "}
+          </PrimaryButton>
+        )}
         <img
           alt="Event"
           loading="lazy"
