471d40aa56499b7bcbdae335783e84a7020546a5
sanjaykumargouda.190183107003@gmail.com <sanjayofficial755@gmail.com>
2021-07-03 22:33:19 -0700

    about section is completed and Blog component is created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/about.page";
 import { Index } from "./pages/index.page";
 import RegisterEvent from "./pages/register-event.page";
-
+import { Blog } from "./pages/blog.page";
 /* 
   3 Pages
   1.index 2.about 3.register-event
@@ -14,6 +14,7 @@ const App = () => {
         <Route exact path="/" component={Index} />
         <Route path="/about" component={About} />
         <Route path="/register-event" component={RegisterEvent} />
+        <Route path="/blog" component={Blog}/>
       </Switch>
     </Router>
   );
--- src/pages/index.page.js
+++ src/pages/index.page.js
@@ -1,7 +1,5 @@
 // Index Page
 
-import { Footer } from "../components/Common/Footer/footer.component";
-import { Header } from "../components/Common/Header/header.component";
 import { PageBase } from "../components/Common/Page-Base/page-base.component";
 import BlockCollections from "../components/Index/Blocks/block-collections.component";
 import { HeroSection } from "../components/Index/Hero/hero-section.component";
