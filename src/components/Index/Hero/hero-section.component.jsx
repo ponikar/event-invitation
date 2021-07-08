d5e1f7a3b965c89f8b7bcd9ef7601ca009baa288
Darshan Ponikar <drsofficial3k@gmail.com>
2021-07-08 14:26:35 +0530

    Changed title

--- src/components/Index/Hero/hero-section.component.jsx
+++ src/components/Index/Hero/hero-section.component.jsx
@@ -3,7 +3,7 @@ import { HeaderTitle } from "../../Common/Typography/header-title.component";
 export const HeroSection = () => {
   return (
     <section className="flex flex-col justify-center mt-24 p-5 items-center">
-      <HeaderTitle title="Beginning of Journey to become Perfect Engineer!" />
+      <HeaderTitle title="Virtual Event to Explore Extraordinary stuff." />
       <AddToCalender className="my-12" />
       <img
         alt="Event"
